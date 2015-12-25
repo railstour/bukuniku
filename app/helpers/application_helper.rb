db118417e257b9f2bedfbe6c450eb591c13171a5
wendy0402 <wendykurniawan92@gmail.com>
2015-12-25 07:29:26 +0700

    customize home index page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,13 @@ module ApplicationHelper
   def active_class(path)
     'active' if request.fullpath == path
   end
+
+  def money_amount(amount)
+    number_to_currency(amount, unit: 'Rp.')
+  end
+
+  def short_summary(words, length, link)
+    summary = words.truncate(length, separator: ' ')
+    "#{summary} #{link_to('see more', link)}".html_safe
+  end
 end
