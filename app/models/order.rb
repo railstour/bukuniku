b0af7d4f39ed7101baac92e93804242abe57fe4c
wendy0402 <wendykurniawan92@gmail.com>
2015-12-26 09:54:45 +0700

    add cart in index

---
+++ app/models/order.rb
@@ -0,0 +1,26 @@
+class Order < ActiveRecord::Base
+  belongs_to :user
+  has_many :order_details
+
+  validates_inclusion_of :status, in: ['shopping']
+
+
+  def self.find_order(id)
+    if id.present?
+      order = find_by(id: id, status: 'shopping')
+      if order.blank?
+        order = create(status: 'shopping')
+      end
+    else
+      order = create(status: 'shopping')
+    end
+    order
+  end
+
+  def add_book_to_cart(book, quantity)
+    order_detail = order_details.find_or_initialize_by(book: book)
+    order_detail.quantity = order_detail.quantity.to_i + quantity
+    order_detail.save
+    order_detail
+  end
+end
