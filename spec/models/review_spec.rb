eed953f6cf15bed26d532f3e0a6afdf108814dad
Adam Pahlevi <adam.pahlevi@gmail.com>
2015-12-28 07:45:49 +0700

    spec for review

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -1,5 +1,44 @@
 require 'rails_helper'
 
 RSpec.describe Review, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) {
+    User.create(
+      name: 'Test User',
+      email: 'test@example.com',
+      password: 'Password01'
+    )
+  }
+
+  let(:book) {
+    Book.create(
+      title: 'Test Book',
+      price: '1000',
+      quantity: 5
+    )
+  }
+
+  let(:review) {
+    Review.create(
+      reviewer: user,
+      book: book,
+      star: 3,
+      text: 'Sample'
+    )
+  }
+
+  it 'has reference to book' do
+    expect(book).to be_persisted
+    expect(review).to be_persisted
+    expect(review.book).to_not be_nil
+    expect(review.book).to be_a Book
+    expect(review.book).to be book
+  end
+
+  it 'has reference to reviewer' do
+    expect(user).to be_persisted
+    expect(review).to be_persisted
+    expect(review.reviewer).to_not be_nil
+    expect(review.reviewer).to be_a User
+    expect(review.reviewer).to be user
+  end
 end
