e04b3b3ee275cec80ff1dba84c4b5fa98b85cf1d
Adam Pahlevi <adam.pahlevi@gmail.com>
2015-12-28 07:44:38 +0700

    create the has_many and belongs_to association for `Review`

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,6 @@
 class Book < ActiveRecord::Base
   validates_presence_of :title, :price
   validates_numericality_of :quantity, greater_than_or_equal_to: 0
+
+  has_many :reviews
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,2 +1,4 @@
 class Review < ActiveRecord::Base
+  belongs_to :reviewer, class_name: User, foreign_key: :reviewer_id
+  belongs_to :book, class_name: Book, foreign_key: :book_id
 end
