e7e5e49e75add9f55b1eb565175a46009c9a2380
Adam Pahlevi <adam.pahlevi@gmail.com>
2015-12-28 09:14:58 +0700

    save the review when form is submitted

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,5 +2,20 @@ class ReviewsController < ApplicationController
   before_action :authenticate_user!
 
   def create
+    text = params[:review][:text]
+    book_id = params[:review][:book_id]
+    book = Book.find(book_id)
+
+    review = Review.new(
+      text: text,
+      book: book,
+      reviewer: current_user,
+      star: 5,
+    )
+
+    review.save!
+    redirect_to book, notice: "Review has been posted"
+  rescue => e
+    redirect_to book, alert: "Unable to post your review"
   end
 end
