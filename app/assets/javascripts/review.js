68992938a0dded980fe87bd850439d15bea70354
Adam Pahlevi <adam.pahlevi@gmail.com>
2016-01-01 07:29:25 +0700

    initialize rating widget

--- app/assets/javascripts/review.js
+++ app/assets/javascripts/review.js
@@ -1,3 +1,20 @@
-# Place all the behaviors and hooks related to the matching controller here.
-# All this logic will automatically be available in application.js.
-# You can use CoffeeScript in this file: http://coffeescript.org/
+$(document).ready(function() {
+  if ($(".book-detail #book-reviews").length > 0) {
+    // create the rating widget
+    $(".book-rating-widget").rateYo({
+      rating: 1.5,
+      numStars: 5,
+      halfStar: true,
+      minValue: 0,
+      maxValue: 5,
+      starWidth: "16px",
+      spacing: "5px"
+    }).on("rateyo.set", function(e, data){
+    })
+
+    // prevent form from submitting when submit button is clicked
+    $(".review-form input[type='submit']").click(function(evt){
+      evt.preventDefault()
+    })
+  }
+})
