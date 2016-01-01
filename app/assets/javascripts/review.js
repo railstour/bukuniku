dc9a5b9760f553187a67d2b908e0d6df62ace1de
Adam Pahlevi <adam.pahlevi@gmail.com>
2016-01-01 07:30:57 +0700

    hide the rating widget by default

--- app/assets/javascripts/review.js
+++ app/assets/javascripts/review.js
@@ -10,11 +10,15 @@ $(document).ready(function() {
       starWidth: "16px",
       spacing: "5px"
     }).on("rateyo.set", function(e, data){
+      $(".book-rating-widget").css("display", "none")
+      $(".book-review-widget").css("display", "block")
     })
 
     // prevent form from submitting when submit button is clicked
     $(".review-form input[type='submit']").click(function(evt){
       evt.preventDefault()
+      $(".book-rating-widget").css("display", "block")
+      $(".book-review-widget").css("display", "none")
     })
   }
 })
