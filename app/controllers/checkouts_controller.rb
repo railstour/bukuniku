35fe1f0e3041b7a8d945644ef8a7a07a319acafc
Adam Pahlevi <adam.pahlevi@gmail.com>
2016-01-03 11:55:48 +0700

    basic foundation for checkout containers and its style

--- app/controllers/checkouts_controller.rb
+++ app/controllers/checkouts_controller.rb
@@ -1,6 +1,12 @@
 class CheckoutsController < ApplicationController
   def new
-    render layout: 'plain'
+    @cart = Order.where(id: session[:cart_id]).first
+
+    if @cart
+      render layout: 'plain'
+    else
+      redirect_to root_path, alert: "You haven't put anything into your cart"
+    end
   end
 
   def create
