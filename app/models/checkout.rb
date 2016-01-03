760efe5bb15b486debc18fa6748c96e6a80c7c05
Adam Pahlevi <adam.pahlevi@gmail.com>
2016-01-04 06:24:01 +0700

    checkout after save calculate for tariffs

--- app/models/checkout.rb
+++ app/models/checkout.rb
@@ -15,6 +15,8 @@ class Checkout < Order
   validates_presence_of :checkout_amount, if: -> { status != STATUS_SHOPPING }
 
   before_save :assign_checkout_time
+  after_save :calculate_shipping_fee
+  after_save :calculate_tax_fee
 
   private
 
@@ -22,5 +24,17 @@ class Checkout < Order
     if checkout_time.blank? && status == WAITING_PAYMENT
       self.checkout_time = DateTime.now 
     end
+  end
+
+  def calculate_shipping_fee
+    shipping_fee = tariffs.fee_type(Tariff::SHIPPING_FEE).first_or_initialize
+    shipping_fee.gross_amount = 10_000_00 # fee is in cents
+    shipping_fee.save!
+  end
+
+  def calculate_tax_fee
+    tax_fee = tariffs.fee_type(Tariff::TAX).first_or_initialize
+    tax_fee.gross_amount = (3.7 * 100) * total_price
+    tax_fee.save!
   end
 end
