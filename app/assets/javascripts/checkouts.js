caca11f14c6faf56172113c349ec82ec7008a4f4
Adam Pahlevi <adam.pahlevi@gmail.com>
2016-01-10 04:35:56 +0700

    peripheral functions to get the CC token

--- app/assets/javascripts/checkouts.js
+++ app/assets/javascripts/checkouts.js
@@ -1,5 +1,30 @@
 $(document).ready(function() {
   if($("#checkout-form-container .payment-form-frame").length > 0) {
     $("#cc_card_number").makeCreditCardField()
+
+    Veritrans.client_key = window.veritransClientKey
+    Veritrans.url = window.veritransBaseUrl + "/token"
+
+    var card = function() { return {
+      'card_number': $("#cc_card_number").val(),
+      'card_exp_month': $("#cc_card_exp_month").val(),
+      'card_exp_year': $("#cc_card_exp_year").val(),
+      'card_cvv': $("#cc_card_cvv").val()
+    }}
+
+    getAllFormFields = function(targetForm) {
+      fields = {}
+      targetForm.find("input[name], textarea").each(function(i,elm){
+        elm = $(elm)
+
+        fieldName = elm.attr("name")
+
+        // do not include credit card data, it's sensitive!
+        if (!(fieldName.indexOf("direct") == 0)) {
+          fields[fieldName] = elm.val()
+        }
+      })
+      return fields
+    }
   }
 })
