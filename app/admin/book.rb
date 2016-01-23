795e655f95e5ade3b811f632bf07352d6e410e14
wendy0402 <wendykurniawan92@gmail.com>
2016-01-23 07:09:13 +0700

    add admin page for book

--- app/admin/book.rb
+++ app/admin/book.rb
@@ -12,4 +12,24 @@ ActiveAdmin.register Book do
   filter :title
   filter :quantity
   filter :author
+
+
+  show do |book|
+    attributes_table do
+      row :id
+      row :title
+      row :quantity
+      row :price
+      row :author
+      row :image
+    end
+
+    panel 'review' do
+      reviews = book.reviews
+      table_for reviews do
+        column('Review') { |review| review.text }
+        column('Star') { |review| review.star }
+      end
+    end
+  end
 end
