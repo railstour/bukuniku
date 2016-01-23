9beef3c3aec61489d4fe1618ea6b3b358a5aec6c
wendy0402 <wendykurniawan92@gmail.com>
2016-01-23 08:39:10 +0700

    add feature to delete review

--- app/admin/book.rb
+++ app/admin/book.rb
@@ -6,6 +6,9 @@ ActiveAdmin.register Book do
     column :price
     column :author
     column :image
+    column 'Total Comments' do |book|
+      book.reviews.count
+    end
     actions
   end
 
@@ -29,7 +32,18 @@ ActiveAdmin.register Book do
       table_for reviews do
         column('Review') { |review| review.text }
         column('Star') { |review| review.star }
+        column('Actions') { |review| link_to 'delete', review_admin_book_path(book.id, review_id: review.id), data: {method: :delete} }
       end
     end
+  end
+
+  member_action 'review', method: :delete do
+    review = Review.find(params[:review_id])
+    if review.destroy
+      flash[:notice] = "Review is destroyed"
+    else
+      flash[:alert] = review.errors.full_messages.to_sentence
+    end
+    redirect_to admin_book_path(params[:id])
   end
 end
